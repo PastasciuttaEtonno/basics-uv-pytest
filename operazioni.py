fda0bb9ceffa8d19f31e7b89ea1a8ad14499e2c4
Ivannn <109225439+PastasciuttaEtonno@users.noreply.github.com>
2026-01-22 09:57:42 +0100

    Update operazioni.py

--- operazioni.py
+++ operazioni.py
@@ -7,3 +7,8 @@ def sottrazione(a,b):
     if isinstance(a,(int,float))  and isinstance(b,(int,float)):
         return a-b
     else: return None
+
+def moltiplicazione(a,b):
+    pass
+def divisione(a,b):
+    pass
